d676ac0ffd104957df628856f8e311fb6b76fd49
Joel Smith <Hikash@gmail.com>
2020-10-27 16:55:50 -0700

    Add v2 config

--- src/appconfig.h
+++ src/appconfig.h
@@ -15,8 +15,10 @@
 
 #include "exceptions/fileerror.h"
 #include "helpers/constants.h"
+#include "config/basic_config.h"
 #include "config/config.h"
 #include "config/config_v1.h"
+#include "config/config_v2.h"
 #include "config/no_config.h"
 
 // AppConfig is a singleton construct for accessing the application's configuration.
@@ -67,7 +69,26 @@ class AppConfig {
                                configFile.error());
     }
 
-    _config = ConfigV1::fromJson(data);
+    int version = -1;
+    try {
+      version = BasicConfig::parseVersion(data);
+    }
+    catch (std::exception &e) {
+      throw std::runtime_error("Unable to parse config file");
+    }
+
+    switch(version) {
+      case 0:
+      case 1:
+        _config = ConfigV1::fromJson(data);
+        break;
+      case 2:
+        _config = ConfigV2::fromJson(data);
+        break;
+    }
+    if (_config == nullptr ) {
+      throw std::runtime_error("Unknown Config file");
+    }
     if (_config->errorText() != "") {
       throw std::runtime_error("Unable to parse config file");
     }
@@ -95,6 +116,13 @@ class AppConfig {
       return false;
     }
     bool upgraded = false;
+    switch(_config->version()) {
+      case 0:
+      case 1:
+        _config = ConfigV2::fromV1(std::move(_config));
+        upgraded = true;
+        // fallthrough (for future upgrades -- v1->v2->v3, etc
+    }
 
     return upgraded;
   }
